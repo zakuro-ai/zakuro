b275d554e30ed7369b0ba8241cfdae9dff655c26
CADIC Jean Maximilien <jcadic@zakuro-ai.com>
2026-02-15 23:33:04 +0900

    build: update build timestamp for v0.2.0 release

--- zakuro/__init__.py
+++ zakuro/__init__.py
@@ -15,7 +15,7 @@ Example:
 """
 
 __version__ = "0.2.0"
-__build__ = "2026-02-12T15:37:02+0900"
+__build__ = "2026-02-15T23:23:17+0900"
 
 from zakuro.compute import Compute
 from zakuro.config import Config
